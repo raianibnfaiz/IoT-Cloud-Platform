0bfbea661351a01bf80725ed78628b7c093a17b6
Mohammad Joynul Abedin <shokal.ash116558@gmail.com>
2025-03-07 10:00:33 +0600

    Refactor project structure and enhance authentication flow
    - Remove NavBar component from original location
    - Update MainLayout to use new NavBar and Footer components from SharedLayoutComponents
    - Modify AuthProvider to redirect to dashboard after successful login
    - Update Root component to remove commented-out NavBar
    - Improve overall project organization and routing

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
-import NavBar from '../NavBar/NavBar'
 
 const Root = () => {
   return (
     <div>
-        {/* <NavBar></NavBar> */}
         <Outlet></Outlet>
     </div>
   )
