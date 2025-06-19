3fda9cf21f3c53c6fe2ebcc036f34860d07f06ca
MahmudMoon <mooncseru14@gmail.com>
2025-06-19 10:15:04 +0600

    Update API base URLs to use local server for development

--- src/config/apiEndpoints.js
+++ src/config/apiEndpoints.js
@@ -1,10 +1,10 @@
 const BASE_URL_LOCAL = 'http://localhost:3000';
-const BASE_URL_REMOTE = 'https://cloud-platform-server-for-bjit.onrender.com';
-const BASE_URL = BASE_URL_LOCAL; // Change this to BASE_URL_REMOTE if you want to use the remote server
+const BASE_URL_REMOTE = 'http://103.197.206.61:3000';
+const BASE_URL = BASE_URL_REMOTE; // Change this to BASE_URL_REMOTE if you want to use the remote server
 
 // WebSocket base URLs
 const WS_BASE_URL_LOCAL = 'ws://localhost:8083';
-const WS_BASE_URL_REMOTE = 'wss://cloud-platform-server-for-bjit.onrender.com:443';
+const WS_BASE_URL_REMOTE = 'ws://103.197.206.61:3000';
 const WS_BASE_URL = BASE_URL === BASE_URL_LOCAL ? WS_BASE_URL_LOCAL : WS_BASE_URL_REMOTE;
 
 const API_ENDPOINTS = {
